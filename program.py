d2df2a2d061f428ee7cb19bddb8a1d6444115ff5
abekele0413 <abekele0413@yahoo.com>
2020-01-02 07:21:50 -0600

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 #python /mnt/c/Users/adri9996/Documents/test5.py
 import getpass
-hashpassword = getpass.getpass(b'keepass password:')
-#hashpassword = b'0'
+#hashpassword = getpass.getpass(b'keepass password:')
+hashpassword = b'0'
 devicenum = raw_input('enter device number:')
 print 'running script....' 
 from cryptography.fernet import Fernet
@@ -13,45 +13,95 @@ cipher_suite = Fernet(key)
 ciphered_text1 = b'gAAAAABeC2GFDDsiJ55to82BZwWVNhgySRW76'
 ciphered_text2 = b'F7jM5aI9FKWAZ3R9I7TQrrN7S04V0BNZzjjYldWi2VpvK-lYgxbEEYkmGGyQ=='
 hashpassword = ciphered_text1+hashpassword+ciphered_text2
-
+#-------------------------checks password------------------------------------
 try:
 	(cipher_suite.decrypt(hashpassword))
 except:
 	print '\n'+'#'*35+'\n####  Incorrect Password Kosta ####'+'\n'+'#'*35
 else:
 	pw = (cipher_suite.decrypt(hashpassword))
-	#pw = raw_input('enter keepass password:')
-	#pw = 'password'
 	kp = PyKeePass('/mnt/c/Users/adri9996/downloads/Database.kdbx', password=pw)
 	device = kp.find_entries(string={'device number': devicenum}, first=True)
-	deviceuser = device.username
-	devicepw = device.password
-	deviceip = device.url
-	devicetitle = device.title
-	enablepw = 'None'
-	enablepw = device.get_custom_property('enable password')
-	enablepw = str(enablepw)
-	devicenumber = device.get_custom_property('device number')
-	customer = device.group
-	customer = str(customer)[7:]
+#-------------------------checks device number------------------------------------
 	try:
-		device.notes
+		device.username
 	except:
-		devicenotes = 'None'
+		print '\n'+'#'*40+'\n####  Incorrect device number Kosta ####'+'\n'+'#'*40 
 	else:
-		devicenotes = str(device.notes)
+		device = kp.find_entries(string={'device number': devicenum}, first=True)
+		deviceuser = device.username
+		devicepw = device.password
+		deviceip = device.url
+		JH = 'None'
+		JH = device.get_custom_property('JH')
+		JH = str(JH)
+		JH = JH.lower()
+		devicetitle = device.title
+		enablepw = 'None'
+		enablepw = device.get_custom_property('enable password')
+		enablepw = str(enablepw)
+		devicenumber = device.get_custom_property('device number')
+		customer = device.group
+		customer = str(customer)[7:]
+		try:
+			device.notes
+		except:
+			devicenotes = 'None'
+		else:
+			devicenotes = str(device.notes)
 	
 	
-	print '\n',40 * '-','\nDevice Number: '+devicenumber,'\nCustomer: '+customer,'\nDevice Name: '+devicetitle, '\nDevice ip: '+deviceip, '\nUsername: '+deviceuser
-	if enablepw == 'None':
-		pyperclip.copy(devicepw)
-		print 'Password: ******* (In your clipboard)'
-	else:
-		pyperclip.copy(enablepw)
-		print 'Password: ******* \nEnable: ******* (In your clipboard)'
+		print '\n',40 * '-','\nDevice Number: '+devicenumber,'\nCustomer: '+customer,'\nDevice Name: '+devicetitle, '\nDevice ip: '+deviceip, '\nUsername: '+deviceuser
+
+		if deviceuser == 'Tacacs':
+			print 'Password: Tacacs'
+			print 'Notes: \n'+devicenotes,'\n',40 * '-'
+			if JH == 'none':
+				deviceuser = raw_input('Enter Tacacs username:')
+				devicepw = getpass.getpass('Enter Tacacs password:')
+				os.system('sshpass -p '+devicepw +' ssh '+deviceuser+'@'+deviceip)
+			else:
+				JumpH = kp.find_entries(title='jumphost', first=True)
+				JumpHip = str(JumpH.url)
+				JumpHusername = str(JumpH.username)
+				JumpHdomain = str(JumpH.get_custom_property('domain'))
+				JumpHnotes = str(JumpH.notes)
+				print '!'*40,'\nMust connect to Jumphost '+JumpHip + '\n','!'*40,'\n',40 * '-'+'\nJH Username: '+JumpHusername+'\nJH Domain: '+JumpHdomain +'\nJH Notes: '+JumpHnotes +  '\n',40 * '-'
+	# need to add the above to the below 
+		elif enablepw == 'None':
+			if JH == 'none':
+				print 'Password: ******* (In your clipboard)'
+				print 'Notes: \n'+devicenotes,'\n',40 * '-'
+				pyperclip.copy(devicepw)
+				os.system('sshpass -p '+devicepw +' ssh '+deviceuser+'@'+deviceip)
+			else:
+				JumpH = kp.find_entries(title='jumphost', first=True)
+				JumpHip = str(JumpH.url)
+				JumpHusername = str(JumpH.username)
+				JumpHdomain = str(JumpH.get_custom_property('domain'))
+				JumpHnotes = str(JumpH.notes)
+				print 'Password: ******* '
+				print 'Notes: \n'+devicenotes,'\n',40 * '-'
+				print '!'*40,'\nMust connect to Jumphost '+JumpHip + '\n','!'*40,'\n',40 * '-'+'\nJH Username: '+JumpHusername+'\nJH Domain: '+JumpHdomain +'\nJH Notes: '+JumpHnotes +  '\n',40 * '-'
+		else:
+			if JH == 'none':
+				pyperclip.copy(enablepw)
+				print 'Password: ******* \nEnable: ******* (In your clipboard)'
+				print 'Notes: \n'+devicenotes,'\n',40 * '-'
+				os.system('sshpass -p '+devicepw +' ssh '+deviceuser+'@'+deviceip)
+			else:
+				JumpH = kp.find_entries(title='jumphost', first=True)
+				JumpHip = str(JumpH.url)
+				JumpHusername = str(JumpH.username)
+				JumpHdomain = str(JumpH.get_custom_property('domain'))
+				JumpHnotes = str(JumpH.notes)
+				print 'Password: ******* \nEnable: ******* '
+				print 'Notes: \n'+devicenotes,'\n',40 * '-'
+				print '!'*40,'\nMust connect to Jumphost '+JumpHip + '\n','!'*40,'\n',40 * '-'+'\nJH Username: '+JumpHusername+'\nJH Domain: '+JumpHdomain +'\nJH Notes: '+JumpHnotes +  '\n',40 * '-'
+
 	
-	print 'Notes: \n'+devicenotes,'\n',40 * '-'
-	os.system('sshpass -p '+devicepw +' ssh '+deviceuser+'@'+deviceip)
+	
+	
 
 
 #----------------------------------------------------------------------------------------
